7ffc4600c5e39d387e29eb32f1b41bc6697a7c5f
sonic1176 <tsonic@gmx.de>
2015-11-22 08:17:14 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,7 @@ library(reshape2)
 library(dplyr)
 
 run_analysis <- function() {
-    # load relevant Labels from csv-Files for activities and features
+    # 1. Load relevant Labels from csv-Files for activities and features
     activityLabel <- read.csv("./UCI HAR Dataset/activity_labels.txt", header=F, sep=" ")
     names(activityLabel) <- c("activityID", "activityText")
     activityLabel$activityText <- sub("_", " ", activityLabel$activityText)
@@ -10,22 +10,22 @@ run_analysis <- function() {
     featureLabel <- read.csv("./UCI HAR Dataset/features.txt", header=F, sep=" ")
     names(featureLabel) <- c("featureID", "featureText")
     
-    # load test datasets for allocation of activity type and subject
+    # 2. Load test datasets for allocation of activity type and subject
     testActivity <- read.csv("./UCI HAR Dataset/test/y_test.txt", header=F)
     names(testActivity) <- c("activityID")
     testActivity <- merge(testActivity, activityLabel)
     testSubject <- read.csv("./UCI HAR Dataset/test/subject_test.txt", header=F)
     names(testSubject) <- c("subjectID")
     
-    # load test data, name them and keep only relevant columns
+    # 3. Load test data, name them and keep only relevant columns
     testData <- read.table("./UCI HAR Dataset/test/x_test.txt", header=F)
     names(testData) <- featureLabel$featureText
     testData <- testData[, grep("std\\(\\)|mean\\(\\)", names(testData))]
     
-    # merge tables to a complete table for Test Data
+    # 4. Merge tables to a complete table for Test Data
     testActivity <- cbind(datasource=c("Test Data"), testActivity, testSubject, testData)
     
-    # repeat for train data - load test datasets for allocation of activity type and subject
+    # 5. Repeat for train data - load test datasets for allocation of activity type and subject
     trainActivity <- read.csv("./UCI HAR Dataset/train/y_train.txt", header=F)
     names(trainActivity) <- c("activityID")
     trainActivity <- merge(trainActivity, activityLabel)
@@ -38,17 +38,17 @@ run_analysis <- function() {
     
     trainActivity <- cbind(datasource=c("Train Data"), trainActivity, trainSubject, trainData)
     
-    # concat datasets
+    # 6. Concat datasets
     cdata <- rbind(trainActivity, testActivity)
     
 
-    # reshape data
+    # 7. Reshape data
     cdata <- melt(cdata, id.vars=c(1:4), measure.vars=(5:70))
     
-    # aggregate data
+    # 8. Aggregate data
     gb <- group_by(cdata, variable, activityID, activityText, subjectID)
     result <- summarize(gb, meanValue=mean(value))
     
-    # write to file
+    # 9. Write to file
     write.table(result, "output.txt", row.names = F)
 }
